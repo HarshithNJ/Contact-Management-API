6b8d530563deba909560fb41c06d79fff1893555
Harshith N J <njharshith122@gmail.com>
2024-12-28 19:01:45 +0530

    Save record completed successfully

--- src/main/java/org/contact/contact/controller/contactController.java
+++ src/main/java/org/contact/contact/controller/contactController.java
@@ -1,13 +1,24 @@
 package org.contact.contact.controller;
 
+import org.contact.contact.dto.contact;
 import org.contact.contact.service.contactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 public class contactController {
 
     @Autowired
     contactService service;
+
+
+    //To Save a record
+    @PostMapping("/contacts")
+    public ResponseEntity<Object> addContact(contact contact){
+        return service.addContact(contact);
+    }
     
 }
--- src/main/java/org/contact/contact/service/contactService.java
+++ src/main/java/org/contact/contact/service/contactService.java
@@ -1,7 +1,13 @@
 package org.contact.contact.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.contact.contact.dto.contact;
 import org.contact.contact.repository.contactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +15,31 @@ public class contactService {
 
     @Autowired
     contactRepository repository;
+
+    public ResponseEntity<Object> addContact(contact contact) {
+        if(repository.existsByName(contact.getName())){
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Name Already Exists");
+
+            if(contact.getPhoneNumber()> 10000000000L){
+
+                Map<String, Object> map1 = new HashMap<String, Object>();
+                map1.put("error", "Length of phone number must not exceed 10 digits");
+   
+                return new ResponseEntity<Object>(map1, HttpStatus.BAD_REQUEST);
+            }
+
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        }else{
+            repository.save(contact);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Data Saved Successfully");
+            map.put("Data", contact);
+
+            return new ResponseEntity<Object>(map, HttpStatus.OK);
+        }
+    }
     
 }

e4645b5d13bb209444b13eb4a72163a795e34b96
Harshith N J <njharshith122@gmail.com>
2024-12-31 15:09:18 +0530

    swagger implementation

--- src/main/java/org/contact/contact/ContactApplication.java
+++ src/main/java/org/contact/contact/ContactApplication.java
@@ -3,7 +3,13 @@ package org.contact.contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Contact Management API", version = "1.0", description = "API Documentation for contact management system", 
+					contact = @Contact(name = "Harshith", email = "Haarshith.dev2024@outlook.com")))
 public class ContactApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/contact/contact/controller/swaggerController.java
@@ -0,0 +1,14 @@
+package org.contact.contact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class swaggerController {
+    
+    @GetMapping("/")
+    public String swagger() {
+        return "redirect:/swagger-ui/index.html";
+    }
+    
+}
